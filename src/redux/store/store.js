0b5ce182fb43bfc1caee70b481e2d1a544ddd740
VietHq <viethq@relipasoft.com>
2022-10-28 16:05:21 +0700

    Begin folder

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../slice/counterSlice';
-import todolistReducer from '../slice/todolistSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
-    todolist: todolistReducer
+    // add reducer
   }
 })
